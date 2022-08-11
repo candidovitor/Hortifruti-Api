ce80483dbb30cb206dea718c77fdb64fd363f514
vitor candido <vitor.cr018@gmail.com>
2022-08-11 12:59:29 -0300

    creating database

--- app.py
+++ app.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.before_first_request
+def cria_banco():
+    banco.create_all()
+
 if __name__ == "__main__":
+    from database import banco
+    banco.init_app(app)
     app.run(debug=True)
---
+++ models/alimentos.py
@@ -0,0 +1,36 @@
+from database import banco
+
+class FrutaModel(banco.Model):
+    __tablename__ = 'informacao'
+
+    fruit_id = banco.Column(banco.Float, primary_key=True)
+    name = banco.Column(banco.String(20))
+    family = banco.Column(banco.String(30))
+    genus = banco.Column(banco.String(40))
+    order = banco.Column(banco.String(40))
+    carbohydrates = banco.Column(banco.Float(precision=2))
+    protein = banco.Column(banco.Float(precision=2))
+    fat = banco.Column(banco.Float(precision=2))
+    calories = banco.Column(banco.Float(precision=2))
+    sugar = banco.Column(banco.Float(precision=2))
+    
+    def __init__(self, fruit_id, name, genus, order, carbohydrates, protein, fat, calories, sugar)
+    self. fruit_id = fruit_id
+    self.name = name
+    self.genus = genus
+    self.order = order
+    self.carbohydrates
+    self.protein = protein
+    self.fat = fat
+    self.calories = calories
+    self.sugar = sugar
+
+    def json(self):
+        return {
+            'fruit_id': self.fruit_id,
+            'name': self.name,
+            'genus': self.genus,
+            'order': self.order,
+        }
+
+    
