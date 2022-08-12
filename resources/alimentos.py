f8f0022c4d8f70aee14884125866c8f56aebe093
vitor candido <vitor.cr018@gmail.com>
2022-08-12 18:17:56 -0300

    ```

--- resources/alimentos.py
+++ resources/alimentos.py
@@ -36,6 +36,35 @@ class Frutas(Resource):
         return {"information": frutas}
 
 class Fruta(Resource):
+    argumentos = reqparse.RequestParser()
+    argumentos.add_argument('name')
+    argumentos.add_argument('family')
+    argumentos.add_argument('genus')
+    argumentos.add_argument('order')
+    argumentos.add_argument('carbohydrates')
+    argumentos.add_argument('protein')
+    argumentos.add_argument('fat')
+    argumentos.add_argument('calories')
+    argumentos.add_argument('sugar')
+
+    dados = argumentos.parse_args()
+
+    nova_fruta = {
+        'fruit_id': fruit_id,
+        'name': dados['name'],
+        'family': dados['family'],
+        'genus': dados['genus'],
+        'order': dados['order'],
+        'carbohydrates': dados['carbohydrates'],
+        'protein': dados['protein'],
+        'fat': dados['fat'],
+        'calories': dados['calories'],
+        'sugar': dados['sugar']
+    }
+
+    frutas.append(nova_fruta)
+    return nova_fruta, 200
+
     def get(self, fruit_id):
         for fruta in frutas:
             if fruta['fruit_id'] == fruit_id:
@@ -43,36 +72,7 @@ class Fruta(Resource):
         return {'message': 'Fruit not found'}, 404
         
     def post(self, fruit_id):
-        argumentos = reqparse.RequestParser()
-        argumentos.add_argument('name')
-        argumentos.add_argument('family')
-        argumentos.add_argument('genus')
-        argumentos.add_argument('order')
-        argumentos.add_argument('carbohydrates')
-        argumentos.add_argument('protein')
-        argumentos.add_argument('fat')
-        argumentos.add_argument('calories')
-        argumentos.add_argument('sugar')
-
-        dados = argumentos.parse_args()
-
-        print(dados)
-        nova_fruta = {
-            'fruit_id': fruit_id,
-            'name': dados['name'],
-            'family': dados['family'],
-            'genus': dados['genus'],
-            'order': dados['order'],
-            'carbohydrates': dados['carbohydrates'],
-            'protein': dados['protein'],
-            'fat': dados['fat'],
-            'calories': dados['calories'],
-            'sugar': dados['sugar']
-        }
-
-        frutas.append(nova_fruta)
-        return nova_fruta, 200
-
+        pass
 
     def put(self, fruit_id):
         pass
